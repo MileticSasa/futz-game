275bdd78369929c195f77dc12cb5bd1845005a0f
MileticSasa <miletic_sasa_88@yahoo.com>
2026-01-10 17:34:01 +0100

    made changes to tackle move

--- scenes/characters/character_states/player_state_tackling.gd
+++ scenes/characters/character_states/player_state_tackling.gd
@@ -1,17 +1,23 @@
 extends PlayerState
 class_name PlayerStateTackling
 
-const TACKLE_DURATION := 200
+const PRIOR_TACKLE_DURATION := 200
+const GROUND_FRICTION := 250.0
 
-var time_start_tackle := Time.get_ticks_msec()
+var time_finish_tackle := Time.get_ticks_msec()
+var is_tackling_complete: bool = false
 
 
 func _enter_tree() -> void:
 	animation_player.play("tackle")
-	time_start_tackle = Time.get_ticks_msec()
 
 
-func _process(_delta: float) -> void:
-	if Time.get_ticks_msec() - time_start_tackle > TACKLE_DURATION:
+func _process(delta: float) -> void:
+	if not is_tackling_complete:
+		player.velocity = player.velocity.move_toward(Vector2.ZERO, GROUND_FRICTION * delta)
+		if player.velocity == Vector2.ZERO:
+			is_tackling_complete = true
+			time_finish_tackle = Time.get_ticks_msec()
+	elif Time.get_ticks_msec() - time_finish_tackle > PRIOR_TACKLE_DURATION:
 		state_transition_requested.emit(Player.State.RECOVERING)
 
