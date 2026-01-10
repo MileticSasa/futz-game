cabd6214f51b8f918c1cc03bfbbd7026e428c0b6
MileticSasa <miletic_sasa_88@yahoo.com>
2026-01-10 19:38:13 +0100

    added carried state with oscillation

--- scenes/ball/ball_states/ball_state.gd
+++ scenes/ball/ball_states/ball_state.gd
@@ -1,16 +1,18 @@
 extends Node
 class_name BallState
 
-signal state_transition_requested(new_state: BallState)
+signal state_transition_requested(new_state: Ball.State)
 
+var animation_player: AnimationPlayer = null
 var ball: Ball = null
 var carrier: Player = null
 var player_detection_area: Area2D = null
 
 
-func setup(context_ball: Ball, context_player_detection: Area2D, context_carrier: Player) -> void:
+func setup(context_ball: Ball, context_player_detection: Area2D, context_carrier: Player, context_anim_player: AnimationPlayer) -> void:
 	ball = context_ball
 	player_detection_area = context_player_detection
 	carrier = context_carrier
+	animation_player = context_anim_player
 
 
